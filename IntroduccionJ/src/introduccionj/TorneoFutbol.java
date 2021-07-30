6704dd28d136c542658075156650857224ed7684
dwn84 <dwn84@hotmail.com>
2021-07-29 21:16:20 -0500

    Actualizar jugadores

--- IntroduccionJ/src/introduccionj/TorneoFutbol.java
+++ IntroduccionJ/src/introduccionj/TorneoFutbol.java
@@ -49,10 +49,10 @@ public class TorneoFutbol {
             "Eibar", "Barcelona"
         };
         int goles[] = {
-            10, 11,
+            30, 11,
             11, 12,
             14, 15,
-            19, 8,
+            29, 8,
             24, 8,
             8, 9,
             9, 9,
@@ -64,26 +64,46 @@ public class TorneoFutbol {
         System.out.println("#\tNombre\t\tEquipo\t\tGoles anotados");
         String saltoH = "\t";
         String saltoH2 = "\t";
+        int maximoGoles = goles[0];
+        int tempMaximo = 0;
+        double sumaTotalGoles = 0;
         for (int i = 0; i < goles.length; i++) {
+            //https://es.wikipedia.org/wiki/Operador_ternario
             //Operador ternario, reemplaza condicional doble
-            saltoH =(jugador[i].length() >= 9)?"\t":"\t\t";
+            saltoH = (jugador[i].length() >= 9) ? "\t" : "\t\t";
             /*
             if (jugador[i].length() >= 9) {
                 saltoH = "\t";
             } else {
                 saltoH = "\t\t";
             }
-            */
-            saltoH2 =(equipo[i].length() <= 7)?"\t\t":"\t";
+             */
+            saltoH2 = (equipo[i].length() <= 7) ? "\t\t" : "\t";
             /*if (equipo[i].length() <= 7) {
                 saltoH2 = "\t\t";
             } else {
                 saltoH2 = "\t";
             }*/
             System.out.println((i + 1) + "\t" + jugador[i] + saltoH + equipo[i] + saltoH2 + goles[i]);
+            //acumular los goles
+            sumaTotalGoles += goles[i];
 
+            //localizar la posición de goles con el valor mas alto
+            if (goles[i] > maximoGoles) {
+                maximoGoles = goles[i];
+                tempMaximo = i;
+            }
+        }
+        double promedio = sumaTotalGoles / goles.length;
+        System.out.println("Goleador del torneo");
+        System.out.println(jugador[tempMaximo] + ": " + equipo[tempMaximo] + ", goles:" + maximoGoles);
+        System.out.println("El promedio de goles es:" + promedio);
+        System.out.println("Jugadores con un total de goles anotados inferior al promedio");
+        for (int i = 0; i < goles.length; i++) {
+            if(goles[i]<promedio){
+                System.out.println(jugador[i]);
+            }
         }
 
     }
-
 }
