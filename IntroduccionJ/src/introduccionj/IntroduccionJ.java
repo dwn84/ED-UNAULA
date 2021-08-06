ea9798213d5d7f54c01ac92b2f1f6f9d287fdff9
dwn84 <dwn84@hotmail.com>
2021-08-05 20:39:34 -0500

    Actualización: Manejo de arreglos

--- IntroduccionJ/src/introduccionj/IntroduccionJ.java
+++ IntroduccionJ/src/introduccionj/IntroduccionJ.java
@@ -38,7 +38,7 @@ public class IntroduccionJ {
         for (int i = 0; i < misDatos.length; i++) {
 
         }
-        */
+         */
         char array[];
         array = new char[7];
         Random r = new Random();
@@ -49,11 +49,77 @@ public class IntroduccionJ {
         }
         //foreach
         //recorrido automático de un arreglo
-        double info[] = new double[]{1.1,2.2,3.3,4.4,5.5,6.6};
+        double info[] = new double[]{2, 5, 2, 7, 9, 3, 1};
         for (double dato : info) {
             System.out.println(dato);
         }
         System.out.println("");
+        //arreglo bidimensional - matriz - tabla
+        //estructura uniforme 
+        int datos[][] = new int[3][4];
+        //estructura irregular
+        int publicaciones[][] = new int[4][];
+        publicaciones[0] = new int[2];
+        publicaciones[1] = new int[4];
+        publicaciones[2] = new int[1];
+        publicaciones[3] = new int[5];
+        //crear e inicializar arreglo matriz
+        int matriz[][] = {{1, 2, 1}, {3, 4, 1}, {5, 6, 1}, {7, 8, 1}};
+        System.out.println("Total de filas del arreglo: " + matriz.length);
+        System.out.println("Total de columnas del arreglo: " + matriz[0].length);
+        //contador i se encarga de controlar indice de filas
+        for (int i = 0; i < matriz.length; i++) {
+            //contador i se encarga de controlar indice de columnas
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print("\t" + matriz[i][j]);
+            }
+            System.out.println("");
+
+        }
+
+        //arreglo tridimensional - cubo
+        int cuboDatos[][][] = new int[3][4][2];
+
+        //mostrar datos originales del arreglo (sin ordenar)
+        for (double dato : info) {
+            System.out.print(dato);
+        }
+        //Invocación o llamado del procedimiento
+        ordenarInsercion(info);
+        System.out.println("");
+        //comprobar el ordenamiento
+        for (double dato : info) {
+            System.out.print(dato);
+        }
     }
 
+    //crear un nuevo método o procedimiento para
+    //que sea invocado o utilizado en el main
+    public static void ordenarBurbuja(double[] arreglo) {
+        System.out.println("Ordenando...");
+        double aux;
+        for (int i = 1; i < arreglo.length; i++) {
+            for (int j = 0; j < (arreglo.length - i); j++) {
+                if (arreglo[j] > arreglo[j + 1]) {
+                    aux = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = aux;
+                }
+            }
+        }
+    }
+
+    public static void ordenarInsercion(double[] arreglo) {
+        double aux;
+        int j;
+        for (int i = 1; i < arreglo.length; i++) {
+            aux = arreglo[i];
+            j = i;
+            while (j > 0 && arreglo[j - 1] > aux) {
+                arreglo[j] = arreglo[j - 1];
+                j--;
+            }
+            arreglo[j] = aux;
+        }
+    }
 }
---
+++ IntroduccionJ/src/introduccionj/TablaClasificacion.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introduccionj;
+
+/**
+ *
+ * @author samaniw
+ */
+public class TablaClasificacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String equipo[] = new String[17];
+        int puntos[] = new int[17];
+        int pjugados[] = new int[17];;
+        int pganados[] = new int[17];;
+
+        equipo[0] = "La Tebaida F. C.";
+        equipo[1] = "Valledupar";
+        equipo[2] = "Universitario de Popayán";
+        equipo[3] = "Unión Magdalena";
+        equipo[4] = "Real Santander";
+        equipo[5] = "Real Cartagena";
+        equipo[6] = "Quindío";
+        equipo[7] = "Pereira";
+        equipo[8] = "Orsomarso SC";
+        equipo[9] = "Llaneros FC";
+        equipo[10] = "Leones";
+        equipo[11] = "Fortaleza C.E.I.F";
+        equipo[12] = "Cúcuta";
+        equipo[13] = "Boyacá Chicó";
+        equipo[14] = "Bogotá FC";
+        equipo[15] = "Barranquilla F.C.";
+        equipo[16] = "Atlético FC";
+        
+        pjugados[0] = 10;
+        pjugados[1] = 12;
+        pjugados[2] = 11;
+        pjugados[3] = 11;
+        pjugados[4] = 10;
+        pjugados[5] = 11;
+        pjugados[6] = 10;
+        pjugados[7] = 11;
+        pjugados[8] = 11;
+        pjugados[9] = 11;
+        pjugados[10] = 11;
+        pjugados[11] = 11;
+        pjugados[12] = 11;
+        pjugados[13] = 11;
+        pjugados[14] = 11;
+        pjugados[15] = 11;
+        pjugados[16] = 10;
+
+        pganados[0] = 5;
+        pganados[1] = 4;
+        pganados[2] = 2;
+        pganados[3] = 4;
+        pganados[4] = 6;
+        pganados[5] = 1;
+        pganados[6] = 5;
+        pganados[7] = 7;
+        pganados[8] = 5;
+        pganados[9] = 6;
+        pganados[10] = 5;
+        pganados[11] = 4;
+        pganados[12] = 3;
+        pganados[13] = 4;
+        pganados[14] = 2;
+        pganados[15] = 5;
+        pganados[16] = 2;
+
+        puntos[0] = 17;
+        puntos[1] = 13;
+        puntos[2] = 10;
+        puntos[3] = 14;
+        puntos[4] = 20;
+        puntos[5] = 6;
+        puntos[6] = 17;
+        puntos[7] = 24;
+        puntos[8] = 18;
+        puntos[9] = 20;
+        puntos[10] = 16;
+        puntos[11] = 16;
+        puntos[12] = 14;
+        puntos[13] = 16;
+        puntos[14] = 8;
+        puntos[15] = 17;
+        puntos[16] = 10;
+        
+        //crear arreglos para calcular y almacenar
+        //los partidos empatados y perdidos
+        
+        //mostrar tabla de posiciones ordenada de mayor a menor
+        //por puntos
+        
+        //mostrar el equipo que más partidos a empatado. 
+
+    }
+    
+}
